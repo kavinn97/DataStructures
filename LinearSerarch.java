14710a4899217fa21745221ac787cd30befd6eef
Kavinn S <kavi2101@global.chainsys.com>
2020-10-20 09:32:28 +0530

    Linearsearch

--- LinearSerarch.java
+++ LinearSerarch.java
@@ -6,22 +6,24 @@ public class LinearSerarch {
 	public static void main(String args[]){
 		int[] a=new int[50];
 		Scanner sc =new Scanner(System.in);
-		System.out.println("Enter n");
-		int n=sc.nextInt();
-		for(int i=0;i<n;i++){
+		System.out.println("Enter number of elements:");
+		int num=sc.nextInt();
+		System.out.println("Enter array elements:");
+		for(int i=0;i<num;i++){
 			a[i]=sc.nextInt();
 		}
-		int k=sc.nextInt();
-		int index=LS(a,n,k);
+		System.out.println("Enter target element:");
+		int target=sc.nextInt();
+		int index=linearSearch(a,num,target);
 		if(index==-1)
-			System.out.println("element not in array");
+			System.out.println("Element not in array");
 		else 
-			System.out.println("element is at "+(index+1));
+			System.out.println("Element is at "+(index+1));
 	}
-	public static int LS(int a[],int l,int k){
+	public static int linearSearch(int a[],int num,int target){
 		int i=0;
-		while(i!=l){
-			if(a[i]==k)
+		while(i!=num){
+			if(a[i]==target)
 				return i;
 				i++;
 		}
